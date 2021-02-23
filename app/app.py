e7f523f349140298df03d9a232470b5113734b62
Juan Camilo Palacio Alcaraz <jpalacio@sistecredito.com>
2021-02-23 07:56:34 -0500

    first endpoint done

--- app/app.py
+++ app/app.py
@@ -1,17 +1,15 @@
 # Import libraries
+from app.training_service.entrypoints.routes.template_route import viewsTemplate
+from . import training_service
 from app.training_service.adapter.mongo_repository.db import mongoengine_proj
 from flask import Flask
 from flask_restful import Api, Resource
 from dotenv import load_dotenv
 from os.path import join, dirname
 from config import DevelopmentConfig
-from . import training_service
 
 
 app = Flask(__name__)
-
-mongo_init = mongoengine_proj
-mongoengine_proj.initialize_db(app)
 
 api = Api(app)
 
@@ -20,6 +18,13 @@ load_dotenv(dotenv_path, '.env')
 app.config.from_object(DevelopmentConfig())
 
 DevelopmentConfig.URI_MONGO(app)
+
+#mongo_init = mongoengine_proj
+mongoengine_proj.initialize_db(app)
+
+init_route = viewsTemplate()
+
+init_route.initialize_routes(api)
 
 if __name__ == '__main__':
     app.run(debug=True)
